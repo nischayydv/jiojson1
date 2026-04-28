ec54451bfc4a997b98dc7a3c4fc0d6b88449b95d
nischayydv <nischay419@gmail.com>
2026-04-28 16:27:07 +0530

    Update index.js

--- scraper/index.js
+++ scraper/index.js
@@ -1,7 +1,7 @@
 const axios = require("axios");
 const fs = require("fs");
 
-const STREAM_URL = "https://netx.streamstar18.workers.dev/zio1";
+const STREAM_URL = "https://binge-jiotv.pages.dev/data/id.json";
 const OUTPUT_FILE = "stream.json";
 
 async function fetchAndSaveJson() {
